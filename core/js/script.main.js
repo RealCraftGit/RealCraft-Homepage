b03df267359cf2a7e5cc52c53977790248d86dc7
malon <support@realcraft.lu>
2023-02-18 11:03:06 +0100

    Structure

---
+++ core/js/script.main.js
@@ -0,0 +1,13 @@
+// Main JavaScript
+//     => Extends the default Scripts
+//     =>
+
+// Navbar responsive
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
