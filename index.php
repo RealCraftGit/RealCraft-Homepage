a5fe1d2305fc416dcee8f4ba3855e0ef337382d4
malon <support@realcraft.lu>
2023-02-18 10:58:13 +0100

    Structure

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="`en`">
+<head>
+    <!--meta-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--styles-->
+    <link rel="stylesheet" href="core/stylesheet/css/style.main.css">
+    <link rel="stylesheet" href="core/Plugins/FontAwesome/css/all.min.css">
+    <!--scripts-->
+    <link rel="script" href="core/js/script.main.js">
+    <!--font-familys-->
+    <link rel="stylesheet" href="core/stylesheet/fonts/style.font-face.css">
+
+    <!--icons-->
+    <link rel="icon" href="core/img/icons/icon-192x192.png">
+    <!--links-->
+    <link rel="manifest" href="manifest.json">
+
+
+
+
+
+    <title>Title</title>
+</head>
+<body>
+
+<header>
+
+</header>
+<main>
+<section id="About-Us">
+    <div class="us-section-header">
+
+
+    </div>
+    <div class="profile-0">
+
+
+    </div>
+    <div class="profile-0">
+
+
+    </div>
+    <div class="profile-0">
+
+
+    </div>
+</section>
+</main>
+
+
+
+
+
+
+
+
+
+<script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "navbar") {
+            x.className += " responsive";
+        } else {
+            x.className = "navbar";
+        }
+    }
+
+</script>
+
+
+</body>
+</html>
